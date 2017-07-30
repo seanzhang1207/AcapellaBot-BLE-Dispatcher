8d977aed8a0298be3e1bccb6cd310d29290ae66d
seanzhang1207 <seanzhang1207@163.com>
2017-07-30 21:44:03 +0800

    Initial Commit

---
+++ AcapellaBot BLE Dispatcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AcapellaBot BLE Dispatcher
+//
+//  Created by Sean Zhang on 30/07/2017.
+//  Copyright © 2017 Sean Zhang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AcapellaBot BLE Dispatcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AcapellaBot BLE Dispatcher
+//
+//  Created by Sean Zhang on 30/07/2017.
+//  Copyright © 2017 Sean Zhang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
